03468e1f488fe8718438dcec6fcb84b41805e817
Ricardo Rocha <me@rrocha.uk>
2024-02-18 00:04:43 +0000

    add colors

---
+++ zsh/colors.zsh
@@ -0,0 +1,28 @@
+# the double escape is because I want to call this variables inside my .zsh file
+
+RESET=\\e[0m
+BOLD=\\e[1m
+DIM=\\e[2m
+INVERT=\\e[3m
+UNDERSCORE=\\e[4m
+STRIKETRHOUGH=\\e[9m
+
+# foreground
+BLACK=\\e[30m
+RED=\\e[31m
+GREEN=\\e[32m
+YELLOW=\\e[33m
+BLUE=\\e[34m
+PURPLE=\\e[35m
+CYAN=\\e[36m
+WHITE=\\e[37m
+
+# background
+BLACKB=\\e[40m
+REDB=\\e[41m
+GREENB=\\e[42m
+YELLOWB=\\e[43m
+BLUEB=\\e[44m
+PURPLEB=\\e[45m
+CYANB=\\e[46m
+WHITEB=\\e[47m
