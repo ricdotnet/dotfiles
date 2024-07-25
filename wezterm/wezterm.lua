b07eb09fadbe1295da2cf1ab8567117eb9b0579d
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-07-25 16:13:38 +0100

    add colors file and update main config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,6 @@ config.color_scheme = 'Catppuccin Mocha'
 config.initial_cols = 150
 config.initial_rows = 35
 
-config.enable_tab_bar = false
 config.window_padding = {
   left = 0,
   right = 0,
@@ -16,9 +15,17 @@ config.window_padding = {
   bottom = 0
 }
 
+-- tabs
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
 -- font config
 config.font = wezterm.font('JetbrainsMono Nerd Font')
 config.font_size = 16
-config.line_height = 1.4
+config.line_height = 1.2
+
+-- colors
+config.window_frame = {}
+config.colors = require('colors')
 
 return config
