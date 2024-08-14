2ab3e6563464aa34491f768154859db56c2ac4fe
Ricardo Rocha <me@rrocha.uk>
2024-08-14 21:48:08 +0100

    update vscode custom theme

--- vscode/custom.js
+++ vscode/custom.js
@@ -1,7 +1,7 @@
 let isCmdClick = false;
 
 window.addEventListener('keydown', (e) => {
-  if (e.key === 'Meta') isCmdClick = true;
+  if (e.key === 'Meta' || e.key === 'Control') isCmdClick = true;
   if (e.key === 'p' && isCmdClick) {
     let timer;
     setTimeout(() => {
@@ -21,19 +21,12 @@ window.addEventListener('keydown', (e) => {
 });
 
 window.addEventListener('keyup', (e) => {
-  if (e.key === 'Meta') isCmdClick = false;
+  if (e.key === 'Meta' || e.key === 'Control') isCmdClick = false;
 });
 
 function overlay() {
   const overlayEl = document.createElement('div');
   overlayEl.id = 'window-overlay';
-  overlayEl.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-  overlayEl.style.position = 'absolute';
-  overlayEl.style.top = '0';
-  overlayEl.style.left = '0';
-  overlayEl.style.width = '100%';
-  overlayEl.style.height = '100%';
-  overlayEl.style.backdropFilter = 'blur(5px)';
 
   return overlayEl;
 }
